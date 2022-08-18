af10854cc4dc8cba18eedca768a09368268a7452
Zenexya <101019279+Zenexya@users.noreply.github.com>
2022-08-18 22:41:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ var app = express();
 app.all('/', function(req, res) {
     res.send("Hello World");
 })
+
+app.listen(3000, function()
+    console.log("[/] Server started listening on port 3000");
+})
